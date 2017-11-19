d08ae1f1752de566682eb7bca0aad98e53238fd3
Carlos Giraldo <carlgira@MacBook-Pro-de-Carlos.local>
2017-11-19 12:58:08 +0100

    model

--- model.py
+++ model.py
@@ -1,21 +1,20 @@
 import pandas as pd
 import numpy as np
-from keras.layers import Conv2D, Flatten, Dense, MaxPooling2D, Lambda, Dropout
+from keras.layers import Conv2D, Flatten, Dense, MaxPooling2D, Lambda, Dropout, Cropping2D
 from keras.models import Sequential
 import keras
 import cv2
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
+max_speed = 30.0
+max_steering_angle = 25.0
 
-def crop_image(image):
-	nimage = image[60:130,]
-	return nimage
 
 def transform_image(image):
-	return crop_image(image)
+	return image
 
-# Data Preprocesing
+
 def prepare_data():
 	driving_log = pd.read_csv('run1/driving_log.csv', names=['center_img', 'left_img', 'right_img', 'steering_angle', 'throttle', 'break', 'speed' ])
 	num_bins = 20
@@ -40,15 +39,15 @@ def prepare_data():
 	for index, row in driving_log_clean.iterrows():
 		# Center Image
 		x_data.append(row.center_img)
-		y_data.append([row.speed, row.steering_angle])
+		y_data.append([row.speed/max_speed, (row.steering_angle)/max_steering_angle])
 
 		# Left Image
 		x_data.append(row.left_img)
-		y_data.append([row.speed, row.steering_angle + 0.25])
+		y_data.append([row.speed/max_speed, (row.steering_angle + 0.25)/max_steering_angle])
 
 		# Right Image
 		x_data.append(row.right_img)
-		y_data.append([row.speed, row.steering_angle - 0.25])
+		y_data.append([row.speed/max_speed, (row.steering_angle - 0.25)/max_steering_angle])
 
 	x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.20)
 	x_test, x_val, y_test, y_val = train_test_split(x_test, y_test, test_size=0.5)
@@ -73,11 +72,11 @@ def data_generator(x_data, y_data, batch_size):
 			yield shuffle(x_data_batch, y_data_batch)
 
 
-
 def nn_model(input_shape):
 
 	model = Sequential()
 
+	model.add(Cropping2D(cropping=((60,20), (0,0)), input_shape=input_shape))
 	model.add(Lambda(lambda x: x/127.5 - 1.0,input_shape=input_shape))
 	model.add(Conv2D(16, kernel_size=(5,5), activation='relu', input_shape=input_shape))
 	model.add(MaxPooling2D(pool_size=(2,2)))
@@ -95,7 +94,7 @@ def nn_model(input_shape):
 
 def main():
 
-	batch_size = 128
+	batch_size = 256
 
 	x_train, x_test, x_val , y_train, y_test, y_val = prepare_data()
 
@@ -106,7 +105,7 @@ def main():
 	model = nn_model(input_shape)
 
 	model.fit_generator(train_generator, samples_per_epoch=len(x_train), validation_data=validation_generator,
-						nb_val_samples=len(x_val), nb_epoch=3)
+						nb_val_samples=len(x_val), nb_epoch=2)
 
 	model.save('model.h5')
 
