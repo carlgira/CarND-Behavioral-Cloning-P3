ad6cf67311358d91e000dfe4dcf347868c00b81a
Carlos Giraldo <carlgira@MacBook-Pro-de-Carlos.local>
2017-11-25 11:40:28 +0100

    model

--- model.py
+++ model.py
@@ -43,18 +43,18 @@ def prepare_data():
 		#y_data.append([row.speed/max_speed, (row.steering_angle)/max_steering_angle])
 
 		# Left Image
-		x_data.append(row.left_img)
+		#x_data.append(row.left_img)
 		#y_data.append([row.speed/max_speed, (row.steering_angle + 0.25)/max_steering_angle])
-		y_data.append(row.steering_angle + 0.25)
+		#y_data.append(row.steering_angle + 0.25)
 
 		# Right Image
-		x_data.append(row.right_img)
+		#x_data.append(row.right_img)
 		#y_data.append([row.speed/max_speed, (row.steering_angle - 0.25)/max_steering_angle])
-		y_data.append(row.steering_angle - 0.25)
+		#y_data.append(row.steering_angle - 0.25)
 
 	x_train, x_val, y_train, y_val = train_test_split(x_data, y_data, test_size=0.20)
 
-	return x_train, x_val , y_train, y_val
+	return x_train, x_val, y_train, y_val
 
 
 def data_generator(x_data, y_data, batch_size):
@@ -65,8 +65,8 @@ def data_generator(x_data, y_data, batch_size):
 
 			images = []
 			for file_name in batch_samples_x:
-				center_image = transform_image(cv2.imread(file_name))
-				images.append(center_image)
+				image = transform_image(cv2.imread(file_name))
+				images.append(image)
 
 			x_data_batch = np.array(images)
 			y_data_batch = np.array(y_data[offset:offset+batch_size])
@@ -80,15 +80,14 @@ def nn_model(input_shape):
 
 	model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=input_shape))
 	model.add(Cropping2D(cropping=((60,20), (0,0))))
-	model.add(Conv2D(16, kernel_size=(5,5), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2,2)))
-	model.add(Conv2D(32, kernel_size=(5,5), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2,2)))
+	model.add(Conv2D(24, (5,5), subsample=(2,2), activation='relu'))
+	model.add(Conv2D(36, (5,5), subsample=(2,2), activation='relu'))
+	model.add(Conv2D(48, (5,5), subsample=(2,2), activation='relu'))
+	model.add(Conv2D(64, (3,3), activation='relu'))
 	model.add(Flatten())
-	model.add(Dense(100, activation='relu'))
-	model.add(Dropout(0.75))
-	model.add(Dense(10, activation='relu'))
-	model.add(Dropout(0.75))
+	model.add(Dense(128, activation='relu'))
+	model.add(Dense(64, activation='relu'))
+	model.add(Dense(8, activation='relu'))
 	model.add(Dense(1))
 
 	model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
@@ -98,7 +97,7 @@ def nn_model(input_shape):
 
 def main():
 
-	batch_size = 256
+	batch_size = 128
 
 	x_train, x_val , y_train, y_val = prepare_data()
 
@@ -117,4 +116,10 @@ if __name__ == '__main__':
 	main()
 
 
-
+# FIX Revisar configuración de 0.25 para las imagenes de la derecha e izquierda --> 0.275 .. 0.25 .. 0.08 .. 0.2
+# Revisar si en udacity se menciona este valor. Revisar si cambiar este valor el tiempo de entrenamiento o el accuracy de conducción
+	
+# FIX Revisar otros modelos de red neural para verificar la activiacion de relu en las capas fcc
+# FIX Quitar warnings de uso de keras 2.0
+# Probar modelo solo con velocidad y asegurar que funciona correctamente
+# Agregar velocidad cuando todo este bien
