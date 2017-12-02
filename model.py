03e37f72d875bab5a6ae1f4200e6047a8e8c3c45
Carlos Giraldo <carlgira@MacBook-Pro-de-Carlos.local>
2017-12-02 17:02:43 +0100

    model

--- model.py
+++ model.py
@@ -102,14 +102,6 @@ def nn_model(input_shape):
 		Dropout(.2),
 		# Convolutional layer 3 48@5x22  | 5x5 kernel | 2x2 stride | elu activation
 		Conv2D(48, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='he_normal', W_regularizer=l2(0.001)),
-		# Dropout with drop probability of .2 (keep probability of .8)
-		Dropout(.2),
-		# Convolutional layer 4 64@3x20  | 3x3 kernel | 1x1 stride | elu activation
-		Conv2D(64, 3, 3, border_mode='valid', activation='elu', subsample=(1, 1), init='he_normal', W_regularizer=l2(0.001)),
-		# Dropout with drop probability of .2 (keep probability of .8)
-		Dropout(.2),
-		# Convolutional layer 5 64@1x18  | 3x3 kernel | 1x1 stride | elu activation
-		Conv2D(64, 3, 3, border_mode='valid', activation='elu', subsample=(1, 1), init='he_normal', W_regularizer=l2(0.001)),
 		# Flatten
 		Flatten(),
 		# Dropout with drop probability of .3 (keep probability of .7)
